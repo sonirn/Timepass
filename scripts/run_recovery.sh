f232044a95a772ab54e644ba49dde6499bf33859
sonirn <sonirn420@gmail.com>
2025-03-26 10:09:11 +0530

    Create run_recovery.sh

---
+++ scripts/run_recovery.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Source Rust environment
+source $HOME/.cargo/env
+
+# Run the Ethereum recovery tool
+./target/release/eth-recovery \
+  --wordlist "$1" \
+  --addresses "$2" \
+  --output "$3" \
+  --checkpoint "$4" \
+  --derivation-paths "$5" \
+  --chunk-size "$6" \
+  --threads "$7" \
+  --checkpoint-interval "$8" \
+  ${9:+--verbose}
